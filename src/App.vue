27c4acc537ccc5b1cb3365f5881c30897e98ed1d
Paul Michael Patena <paulmichaelpatena@gmail.com>
2021-05-04 04:44:07 +1000

    update link to repo

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
         <v-icon>mdi-brightness-6</v-icon>
       </v-btn>
 
-      <v-btn icon title="source code for this project" href="https://github.com/PaulPatena/personal_site_2021" target="_blank">
+      <v-btn icon title="source code for this project" href="https://github.com/PaulPatena/camp" target="_blank">
         <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
