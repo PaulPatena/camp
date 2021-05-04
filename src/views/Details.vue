1367055eb9c82793678e2ac089fab9d07dba587c
Paul Patena <paulp@familyzone.com>
2021-05-04 13:58:36 +1000

    added tabs on details

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -6,12 +6,7 @@
       <v-col cols="12" sm="9">
         <WarningAlert v-if="getDetailedViewIdx===undefined" message="Please select a survey in home page."/>
         <LoadingCard v-else-if="getDetailedSurvey==='loading'"/>
-        <v-card v-else
-          class="pa-2 mb-2" :class="{'light-background': getDarkMode}"
-        >
-          <SurveySummary :survey="getDetailedSurvey"/>
-          <SurveyThemes :themes="getDetailedSurvey.themes"/>
-        </v-card>
+        <DetailTabs v-else/>
       </v-col>
     </v-row>
   </v-container>
@@ -20,19 +15,17 @@
 <script>
 import { mapActions, mapGetters } from 'vuex';
 import LoadingCard from '@/components/LoadingCard.vue';
-import SurveySummary from '@/components/SurveySummary.vue';
 import WarningAlert from '@/components/WarningAlert.vue';
 import HomeButton from '@/components/HomeButton.vue';
-import SurveyThemes from '@/components/SurveyThemes.vue' 
+import DetailTabs from '@/components/DetailTabs.vue'
 
 export default {
   name: 'Details',
   components: {
     LoadingCard,
-    SurveySummary,
     WarningAlert,
     HomeButton,
-    SurveyThemes,
+    DetailTabs,
   },
   computed: {
     ...mapGetters(['getDarkMode', 'getDetailedViewIdx', 'getDetailedSurvey']),
